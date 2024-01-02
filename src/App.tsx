7a6bd35f45be5118340395eadba24421c198e6ff
Saymon Araújo <67705166+saymon-araujo@users.noreply.github.com>
2024-01-02 09:49:03 -0300

    chore: project initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./global.css";
+import { RouterProvider } from "react-router-dom";
+import { HelmetProvider, Helmet } from "react-helmet-async";
+
+import { router } from "./routes";
+
+export function App() {
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={router} />;
+    </HelmetProvider>
+  );
+}
